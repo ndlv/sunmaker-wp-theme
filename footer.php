a2438c4c300b4650c3ae88a3f561bcbe93f45410
Nathan Delavictoire <nathan.delavictoire@gmail.com>
2015-02-03 07:12:10 +0100

    Initial Commit, with begining of the wordpress theme creation project.

---
+++ footer.php
@@ -0,0 +1,24 @@
+      <hr>
+
+      <div class="row">
+          <footer>
+              <p>&copy; Company 2012</p>
+          </footer>
+          <?php wp_footer(); ?>
+      </div>
+
+
+
+    </div> <!-- /container -->
+
+
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="./assets/bootstrap/js/jquery-2.1.1.js"></script>
+    <script src="./assets/bootstrap/dist/js/bootstrap.js"></script>
+
+
+  </body>
+</html>
